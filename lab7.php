5e2f35cc465eda5eae339cea0aeb31d944a55407
Neil Uri Nowgaonkar <nowgaonkarneil@gmail.com>
2022-05-04 21:55:09 +0530

    Completed Lab 7

--- lab7.php
+++ lab7.php
@@ -2,7 +2,6 @@
     include 'conn7.php' 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,15 +9,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-     <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="lab7.css">
 
     <title>Marks Calculator</title>
+
+    <h1 class="heading">Marks Calculator</h1>
+
 </head>
 <body>
 
 <form method="post">
-    <button type="submit" class="btn btn-primary" name="submit">CO1 greater than 60%</button>
+
+
+    <div class="formele">
+    <div class="row g-5">
+
+    <div class="col-sm-5">
+        <div class="form-group">
+            <label>Threshold Marks.</label>
+            <input type="number" class="form-control" placeholder="Enter Threshold Marks = " name="threshold_marks">
+        </div>
+    </div>
+
+    <div class="col-sm-5">
+
+        <div class="form-group">
+            <label>Column Name</label>
+            <input type="text" class="form-control" placeholder="Ener Column No."name ="co_no">
+        </div>
+    </div>
+</div>
+    </div>
+    
+        <div class="showResults">
+        <button type="submit" class="btn btn-primary" name="submit">Show Results</button>
+        </div>
+</div>  
+    
 </form>
 
 <table class="table table-striped table-bordered">
@@ -28,7 +58,7 @@
         <th scope="col">Sr No.</th>
         <th scope="col">Roll No.</th>
         <th scope="col">Student Name</th>
-        <th scope="col">CO1</th>
+        <th scope="col">Column</th>
         </tr>
     </thead>
     <tbody>
@@ -37,7 +67,15 @@
 
         if(isset($_POST['submit']))
         {
-            
+            $count=0;
+            $total_count=0;
+            $per=0;
+            $threshold_marks=$_POST['threshold_marks'];
+            $co_no=$_POST['co_no'];
+            echo '<b>Threshold Marks = '.$threshold_marks;
+            echo '<br>';
+            echo 'Column Name = '.$co_no;
+
             $sql="Select * from mse_ese_co_marks";
             $result=mysqli_query($con,$sql);
 
@@ -49,9 +87,9 @@
                     $sn=$row['SN'];
                     $rn=$row['RN'];
                     $studname=$row['StudentName'];
-                    $marks=$row['CO1'];
+                    $marks=$row[$co_no];
                     
-                    if(($marks/10.5)*100 >= 60)
+                    if(($marks/10.5)*100 >= $threshold_marks)
                     {
                         echo '
                         <tr>
@@ -61,8 +99,18 @@
                         <td>'.$marks.'</td>
                         </tr>
                     ';
+                        $count++;
                     }
+                    $total_count++;
                 }
+                echo '<br>';
+                echo 'Number of Students With Matching Criteria = '.$count;
+                $per=($count/$total_count)*100;
+                echo '<br>';
+                echo 'Percentage of Students with matching criteria = '.$per.'%' ;
+                echo '<br>';
+                echo '<br>';
+
             }
             else
             {
